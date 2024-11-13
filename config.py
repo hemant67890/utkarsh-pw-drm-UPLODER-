df59bf1fe9b886cd9a7f773e5ff7c90544c6133b
hemant67890 <godarasubhash337@gmail.com>
2024-11-13 22:45:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
-API_ID = API_ID = 280367
+API_ID = API_ID = "26512964"
 
-API_HASH = os.environ.get("API_HASH", "c0d4308321c03b82a27b0c9966ceaa")
+API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7299329291:AdpHa1_fFpWa0f-2YXDf8XXaOlsTmugzY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8023334352:AAF0JovhXU2GOhBRK1sOSz7nV0AjHIF7g_s")
 
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
+PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://weloxa8533:WKmtuOzgMtTFGCrC@cluster0.jrz7hfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 
-OWNER = int(os.environ.get("OWNER", 672225212))
+OWNER = int(os.environ.get("OWNER", 6326227068))
 STRING = int(os.environ.get("STRING", BAG22S8ASUEUIC6ypgBZy3PLYWgxb9izOzf4O7ULnCXU0MnsYt9tpLePEPWueFFlqS3dBghQ40t-_TpXuTbxk1bBz7TXSR0Y-G5FfIuUVoj40YdLIOfCP87hsJ7ZTAFURq5ckQjVmpW2MBWfYH_PvY59CP_0efVNYwP0f1N_8akjuvkoxbstaewk65wvdT0t5B3t0d3VDrUKfN1RZvcbvbhKAFr5XD_4Z0NCvDnFWa8JYjlOK3qaSVVgPL23XkxgoW1S9KbjCDO3aTkfpWSWqreGuO6VqdQjxPT0mI0iiXtZ7hB-RP7LW4jQMrkVLJjcAAAAAGQrW58AA))
 
-LOG = -1002120460486
+LOG = "-1002369795937"
 
