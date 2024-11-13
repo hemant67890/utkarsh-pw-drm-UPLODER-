4aa668cc728da9cff946dc7b180ab9b2b72fbe0e
hemant67890 <godarasubhash337@gmail.com>
2024-11-13 22:49:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8023334352:AAF0JovhXU2GOhBRK1sOSz7nV0Aj
 PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://weloxa8533:WKmtuOzgMtTFGCrC@cluster0.jrz7hfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 
 OWNER = int(os.environ.get("OWNER", 6326227068))
-STRING = int(os.environ.get("STRING", BAG22S8ASUEUIC6ypgBZy3PLYWgxb9izOzf4O7ULnCXU0MnsYt9tpLePEPWueFFlqS3dBghQ40t-_TpXuTbxk1bBz7TXSR0Y-G5FfIuUVoj40YdLIOfCP87hsJ7ZTAFURq5ckQjVmpW2MBWfYH_PvY59CP_0efVNYwP0f1N_8akjuvkoxbstaewk65wvdT0t5B3t0d3VDrUKfN1RZvcbvbhKAFr5XD_4Z0NCvDnFWa8JYjlOK3qaSVVgPL23XkxgoW1S9KbjCDO3aTkfpWSWqreGuO6VqdQjxPT0mI0iiXtZ7hB-RP7LW4jQMrkVLJjcAAAAAGQrW58AA))
+STRING = int(os.environ.get("STRING", BQGUjkQAbbJ7ah4mCjlhMh_ieiIj9nozemiT7wV7oXndPHU9NXSwAAMrPkI0Jmd5DZj1M2opZ7lbmhn_rwUVTAwRWWy-IeAogPBdpPk1xwj53sObo6EtqCeDYsgZ3S2Anf9xsKl602nU6pHz8Cn3swT-oUOeXvVzndbGigE_qPJMPqfuvdOKP_t1cMcq5yzmK4LO6lQS2p5SggHr8aF38uxJjyJTQdW4sj_ImNFt6mXiEHwTcE_DwmXYCTJ95Nl5Lz2KuuHSUGEVFNhWmHCnIDSP4E62_sRG-JeA_26Na7Rjg8Ewez9Gulk04QqFDwBPGdmeJzuH3HC7-zKsF2kt3RA_GQuoqQAAAAF5EpB8AA))
 
 LOG = "-1002369795937"
 
